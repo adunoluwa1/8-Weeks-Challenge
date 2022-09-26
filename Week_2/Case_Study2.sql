95fe91cb1a1332e5a9332d91da6de4349d6afa85
Adunoluwa Olutola <adunoluwa1@yahoo.com>
2022-09-26 19:48:48 -0400

    commit

--- Week_2/Case_Study2.sql
+++ Week_2/Case_Study2.sql
@@ -846,6 +846,7 @@
         LEFT JOIN Runner_Ratings r
         ON r.Order_id = o.Order_id
         WHERE cancellation IS NULL
+        
 
     -- 5. If a Meat Lovers pizza was $12 and Vegetarian $10 fixed prices with no cost for extras and each runner is paid $0.30 per kilometre traveled - how much money does Pizza Runner have left over after these deliveries?
             SELECT SUM(s.Revenue + s.[Delivery Charge]) AS [Total Revenue]
